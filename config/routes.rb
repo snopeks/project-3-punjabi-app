7c67f28064c7d457d8461814d80726af96dca442
snopeks <creativesnopeks@gmail.com>
2017-12-05 22:00:23 -0800

    add db, routes, conditional rendering

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   root 'statics#index'
 
+  get '/login', to: "sessions#new"
+  post '/sessions', to: "sessions#create"
+  get '/logout', to: "sessions#destroy"
+
+
+  get '/signup', to: "users#new", as: "new_user"
+  post '/users', to: "users#create"
+  get '/profile', to: "users#show", as: "user"
+  get '/users/:id/edit', to: "users#edit", as: "edit_user"
+  put '/users/:id', to: "users#update", as: "update_user"
+  delete '/users/:id', to: "users#destroy", as: "delete_user"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
