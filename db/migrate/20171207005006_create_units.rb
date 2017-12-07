1a3ee7414d1fa1bfc7b92a6f7eaac095c54588f2
snopeks <creativesnopeks@gmail.com>
2017-12-06 23:26:50 -0800

    add unit db and mockup report card

---
+++ db/migrate/20171207005006_create_units.rb
@@ -0,0 +1,10 @@
+class CreateUnits < ActiveRecord::Migration[5.1]
+  def change
+    create_table :units do |t|
+      t.string :title
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206040549) do
+ActiveRecord::Schema.define(version: 20171207005006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "units", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
