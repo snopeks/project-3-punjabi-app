95f71eb1f11f112cd63335fb8852d52a361cc026
snopeks <creativesnopeks@gmail.com>
2017-12-07 16:35:34 -0800

    update database migration

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'statics#index'
 
-  get '/modules', to: "units#index"
+  get '/modules', to: "units#index", as: "modules"
   get '/modules/:id', to: "units#show"
 
   get '/login', to: "sessions#new"
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
   get '/signup', to: "users#new", as: "new_user"
   post '/users', to: "users#create"
   get '/profile', to: "users#show", as: "user"
-  get '/users/:id/edit', to: "users#edit", as: "edit_user"
-  put '/users/:id', to: "users#update", as: "update_user"
+  get '/profile/edit/:id', to: "users#edit", as: "edit_user"
+  put '/profile/:id', to: "users#update", as: "update_user"
   delete '/users/:id', to: "users#destroy", as: "delete_user"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
